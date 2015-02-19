a4b4bf5c0019b0e79824b750bf81ba9fbb0ef7b7
LuXiangrong <luxiangrongwave@gmail.com>
2015-02-19 22:32:52 +0800

    修改grunt配置，删除一些task。准备了一个文件索引

--- Gruntfile.js
+++ Gruntfile.js
@@ -429,9 +429,9 @@ module.exports = function (grunt) {
     'uglify',
     'copy:dist',
     'modernizr',
-    'rev',
+    //'rev',
     'usemin',
-    'htmlmin'
+    //'htmlmin'
   ]);
 
   grunt.registerTask('default', [
